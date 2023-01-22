c757074bef55f98b557037715af21a277cdda076
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-01-21 21:25:42 -0800

    chore: removed old unused code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,27 +14,4 @@ fn main() {
     );
 
     menu.serve().unwrap();
-
-    cartographer::scream_to_the_void();
 }
-
-// I wish to be able to say:
-// #[cartographer::Menu]
-// struct MainMenu {
-//
-// #[menu_item]
-// Add
-//
-// #[menu_item,
-//     alternative_searches=["UnAdd", "Remove", "stop adding"]
-// Reset
-//
-// #[menu_item,
-//     hidden_from_startmenu]
-// Branches
-//
-// }
-//
-// user_item: MainMenuEnum = MainMenu::serve()?;
-//
-// match user_item
