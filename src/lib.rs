f89ff34f4a49164d9bb179232d914d602462248a
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-01-21 21:25:27 -0800

    docs: added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
 //! A menu builder for Terminal User Interfaces with searching and arrow
 //! key navigation
-
+//! 
 //! ## Example
 //! ```
-//! assert!(true == true) # because I have no working api yet
+//! use cartographer::{menu, menu_item};
+//! let menu = menu! {
+//!       "Pick a number: ",
+//!       [
+//!           menu_item!("Item Number 1", true, 1),
+//!           menu_item!("Item Number 2", false, 2, ["death"]),
+//!           menu_item!("Item Number 3", true, 3),
+//!           menu_item!("Item Number 5", true, 5, ["80", "5"])
+//!       ]
+//!   );
+//! }
+//! 
+//! // Returns the string of the item given
+//! let usr_selection = menu.serve().unwrap();
 //! ```
 //!
 
@@ -17,6 +30,3 @@ pub use menu_struct::{Menu, MenuItem, MenuOptions};
 /// Contains the menu! and menu_item! macros
 mod menu_macros;
 
-pub fn scream_to_the_void() {
-    println!("AHAAAAAAAAAAAAAAA");
-}
