acdec27257e4e4c4bd4500d56bec72a94f01d386
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-01-22 16:29:22 -0800

    docs: Added Docs for menu structs; added serde feature
    Added additional documentation for the struct members of Menu, MenuItem, and MenuOptions
    
    Added feature-driven derivation of Serialize and Deserialize of MenuItem (because console::Key is not Serializable)

--- src/menu_struct.rs
+++ src/menu_struct.rs
@@ -1,25 +1,60 @@
 use console::Key;
 
-#[derive(Clone, Debug)]
-/// The data struct for menu items
-/// Can be build using the `menu_item!` macro
+#[cfg_attr(
+    feature = "serde_serialize",
+    derive(serde::Serialize, serde::Deserialize)
+)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+/// A data structure representing a line-item in a menu
+///
+/// The recommended way of constructing these is to use  the [`menu_item!`] macro
+/// though the output will be the same
+///
+/// ## Example
+/// ```
+/// let menu_item = MenuItem {
+///     visible_name: "A Menu Item".to_string(),
+///     visible_at_rest: true,
+///     at_rest_position: Some(1),
+///     alternative_matches: None,
+///     }
+///
+///
+/// ```
 pub struct MenuItem {
+    /// The String that will display for this item in the menu
     pub visible_name: String,
+
+    /// Toggles if this item will be shown when no search terms are available
     pub visible_at_rest: bool,
-    pub alternative_matches: Option<Vec<String>>,
+
+    /// Optional feature that will let you specify in what order the MenuItems will be
+    /// displayed
     pub at_rest_position: Option<usize>,
+
+    /// A list of strings that will also be used, in addition to the [`visible_name`],
+    /// when processing the search results
+    pub alternative_matches: Option<Vec<String>>,
 }
 
 /// The Menu struct that contains the information and
 /// functions for displaying the menus
+#[derive(Clone, Debug, PartialEq)]
 pub struct Menu {
+    /// The text to be displayed on the same line as user input will be shown.
+    /// To make it extra clear, try adding a semicolon and a space. (e.g. `prompt: "Pick and item: "`)
     pub prompt: String,
+
+    /// The Vector of [`MenuItem`]s
     pub items: Vec<MenuItem>,
+
+    /// The [`MenuOptions`] to use when displaying the menu
     pub configuration: MenuOptions,
 }
 
 /// Controls and characters that can be configured
 /// to change the way the menu acts and displays
+#[derive(Clone, Debug, PartialEq)]
 pub struct MenuOptions {
     /// The user's cursor while they navigate
     pub cursor: char,
