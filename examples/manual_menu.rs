ac8fdf170e07ab704ef2127d16b0761d8714b90c
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-01-25 17:05:44 -0800

    docs: Added empty example files

---
+++ examples/manual_menu.rs
@@ -0,0 +1,16 @@
+use cartographer::{Menu, MenuItem};
+
+fn main() {
+    let mut menu_items: Vec<MenuItem> = Vec::new();
+
+    menu_items.push(MenuItem::new("You can".to_string()));
+    menu_items.push(MenuItem::new("Manually".to_string()));
+    menu_items.push(MenuItem::new("Make A".to_string()));
+    menu_items.push(MenuItem::new("Menu with".to_string()));
+    menu_items.push(MenuItem::new("Pure Structs".to_string()));
+
+    let menu = Menu::new("It is wordy though: ".to_string(), menu_items, None);
+
+    let selection = menu.serve().unwrap().unwrap();
+    println!("{:?}", selection);
+}
