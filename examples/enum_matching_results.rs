7239e7dd12a226759183715fd22564867abca4b6
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-01-25 19:07:34 -0800

    docs: Added example of enum to and from string

---
+++ examples/enum_matching_results.rs
@@ -0,0 +1,51 @@
+use cartographer::{menu, menu_item, MenuOptions};
+use enum_variants_strings::EnumVariantsStrings;
+
+#[derive(Debug, PartialEq, EnumVariantsStrings)]
+enum MenuElements {
+    First,
+    Second,
+    Third,
+    Fourth,
+}
+
+fn main() {
+    let options = MenuOptions::new().cursor('➤').selected_indicator('✓');
+
+    let menu = menu!(
+        "Using serde for good matching: ",
+        options,
+        [
+            menu_item!(MenuElements::First.to_str()),
+            menu_item!(MenuElements::Second.to_str()),
+            menu_item!(MenuElements::Third.to_str()),
+            menu_item!(MenuElements::Fourth.to_str())
+        ]
+    );
+
+    let usr_selection = menu.serve().unwrap().unwrap();
+    let mut usr_enum: Vec<MenuElements> = Vec::new();
+
+    for i in usr_selection {
+        usr_enum.push(MenuElements::from_str(i.as_str()).unwrap());
+    }
+
+    println!("");
+
+    for i in usr_enum {
+        match i {
+            MenuElements::First => {
+                println!("You chose the first one!");
+            }
+            MenuElements::Second => {
+                println!("The second one was chosen");
+            }
+            MenuElements::Third => {
+                println!("3333333333333");
+            }
+            MenuElements::Fourth => {
+                println!("Being last isn't that bad");
+            }
+        }
+    }
+}
